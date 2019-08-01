1a3ab5815400fc403a0c2d52dea0aeb46e1f0b9c
Maxim Manzhos <maxim.mnzh@gmail.com>
2019-08-01 20:32:15 +0300

    Not added checking aircrafts count. Fix it

--- Experiments/src/Loader.java
+++ Experiments/src/Loader.java
@@ -1,9 +1,11 @@
 import com.skillbox.airport.Airport;
+import com.skillbox.airport.Terminal;
 
 public class Loader {
     public static void main(String[] args) {
         Airport airport = Airport.getInstance();
         System.out.println(airport.getAllAircrafts());
+        System.out.println();
     }
 
 }
